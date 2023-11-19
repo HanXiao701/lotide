16160dd4613338b0709d7ea14682fb9d05f1fd54
HanXiao701 <hanxiao701@gmail.com>
2023-11-19 22:36:02 +0100

    assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Paased: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Listhouse Labs", "Listhouse Labs");
+assertEqual("Listhouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 1.5);
